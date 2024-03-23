5383b8c1104af1ad32f5b8a0e5a9641d993d4b75
kapilmogre1998 <kapil.mogre@nobroker.in>
2024-03-23 20:26:54 +0530

    remove addcssfile import

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import addWebpFile from "./addWebpFile";
 import addSvgFile from "./addSvgFile";
 import addTextFile from "./addTextFile";
-import addCSSFile from "./addCSSFile";
 
-addCSSFile();
-addTextFile();
-addSvgFile();
 addWebpFile();
+addSvgFile();
+addTextFile();
