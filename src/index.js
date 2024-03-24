f16d1da9913e00cb7272062e031adea653a44978
kapilmogre1998 <kapil.mogre@nobroker.in>
2024-03-24 13:58:07 +0530

    celan webpack setup

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import addWebpFile from "./addWebpFile";
 import addSvgFile from "./addSvgFile";
 import addTextFile from "./addTextFile";
 import addCSSFile from "./addCSSFile";
+import addSCSSFile from "./addSCSSFile";
 
+addSCSSFile();
 addCSSFile();
 addTextFile();
 addSvgFile();
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,26 +1,23 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 // we can't use ECMAScript here
 module.exports = {
     entry: './src/index.js',
     output: {
-        filename: 'bundler.[contenthash].js',
+        filename: 'bundler.js',
         path: path.resolve(__dirname, 'dist')
     },
 
-    mode: "none",
+    mode: "development",
 
-    //Dev server
     devServer: {
         static: {
             directory: path.join(__dirname, 'dist'),
-          },
-          port: 9000,
+        },
+        port: 9000,
     },
 
-    //asset modules and asset management
     module: {
         rules: [
             // <-- combination of asset/resource + asset/inline -->
@@ -29,11 +26,9 @@ module.exports = {
                 type: "asset",
                 parser: {
                     dataUrlCondition: {
-                    // If the file size is below or equal to 60 KB, it will be embedded as a data URL.
-                    // If the file size is above 60 KB, a separate asset file will be created and referenced in the output HTML.
-                      maxSize: 60 * 1024
+                        maxSize: 4 * 1024 // 4kb
                     }
-                  }
+                }
             },
             {
                 test: /\.txt/,
@@ -46,12 +41,7 @@ module.exports = {
         ]
     },
 
-    //create html file in build process
     plugins: [
-        new HtmlWebpackPlugin({
-            title: 'react-scratch-app',
-            filename: 'index.html'
-        }),
-        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin()
     ]
 }
